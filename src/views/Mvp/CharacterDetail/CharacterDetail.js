2348a45dfd5c768cc73acb1104f73bceb6f08460
Jorge Vicens Valor <gromit.jvv@gmail.com>
2022-01-23 14:53:16 +0100

    wip: characterDetail

--- src/views/Mvp/CharacterDetail/CharacterDetail.js
+++ src/views/Mvp/CharacterDetail/CharacterDetail.js
@@ -3,6 +3,7 @@ import { useHistory, useParams } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Table from 'react-bootstrap/Table'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
@@ -43,49 +44,137 @@ function CharacterDetail() {
     }
   }, [characterReducerData])
 
+  console.log(characterData)
+
+  //   characterData.occupation.map((occupation, index) => {
+  //     return index === characterData.occupation.length - 1 ? '' : ', '
+  //   })
+  // }
+
   return (
     <Container className='d-flex min-vh-100'>
       <div className='mt-4'>
         <Row className='mt-5 mb-3'>
           <div className='d-flex justify-content-between align-items-center'>
-            <h1 className='home-title text-center'>testing</h1>
+            <h1 className='home-title text-center'>Character Details</h1>
             <GenericButton onClick={history.goBack} variant='primary' contents={i18n[`uiTexts${LSLang}`].goBack} />
           </div>
         </Row>
         {/* {error ? <h1>error</h1> : !isLoaded ? <h1>loading...</h1> : <h1>fulfilled</h1>} */}
-        <p>lo veo?</p>
         {error ? (
           <p className='home-title text-center'>ERROR!</p>
         ) : !isLoaded ? (
-          <Row>
-            <Col xs={12} sm={12} md={3} lg={3} xl={3} xxl={3}>
-              <Card>
-                <div style={{ color: 'grey', width: '100%', height: '50%' }} />
+          <p>loading content...</p>
+        ) : (
+          <Row xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
+            <Col className='mb-3'>
+              <Card className='h-100'>
+                <Row className='g-0'>
+                  <Col xs={12} sm={12} md={12} lg={3} xl={3} xxl={3}>
+                    <Card.Img
+                      rounded
+                      variant='top'
+                      className='m-2'
+                      src={characterData.img}
+                      alt={`Picture of ${characterData.img}`}
+                    />
+                  </Col>
+                  <Col xs={12} sm={12} md={12} lg={9} xl={9} xxl={9}>
+                    <Card.Body className='d-flex flex-column'>
+                      <Row>
+                        <Col xs={12} sm={12} md={12} lg={3} xl={3} xxl={3} className='d-flex justify-content-end'>
+                          <span className='text-muted'>Name:</span>
+                        </Col>
+                        <Col xs={12} sm={12} md={12} lg={9} xl={9} xxl={9} className='p-0'>
+                          {characterData.name}
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col xs={12} sm={12} md={12} lg={3} xl={3} xxl={3} className='d-flex justify-content-end'>
+                          <span className='text-muted'>Nickname:</span>
+                        </Col>
+                        <Col xs={12} sm={12} md={12} lg={9} xl={9} xxl={9} className='p-0'>
+                          {characterData.nickname}
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col xs={12} sm={12} md={12} lg={3} xl={3} xxl={3} className='d-flex justify-content-end'>
+                          <span className='text-muted'>Birthday:</span>
+                        </Col>
+                        <Col xs={12} sm={12} md={12} lg={9} xl={9} xxl={9} className='p-0'>
+                          {characterData.birthday}
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col xs={12} sm={12} md={12} lg={3} xl={3} xxl={3} className='d-flex justify-content-end'>
+                          <span className='text-muted'>
+                            {characterData.occupation.length > 1 ? 'Occupations:' : 'Occupation:'}
+                          </span>
+                        </Col>
+                        <Col xs={12} sm={12} md={12} lg={9} xl={9} xxl={9} className='p-0'>
+                          {characterData.occupation.length > 1
+                            ? characterData.occupation.map((occupation, index) => (
+                                <span>
+                                  {occupation}
+                                  {index === characterData.occupation.length - 1 ? '' : ', '}{' '}
+                                </span>
+                              ))
+                            : characterData.occupation}
+                        </Col>
+                      </Row>
+                      {/* <Button
+                    variant='primary'
+                    as={Link}
+                    to={`/mvp/character-detail/${character.char_id}`}
+                    className='mt-auto '
+                  >
+                    {i18n[`uiTexts${LSLang}`].characterDetail}
+                  </Button> */}
+                    </Card.Body>
+                  </Col>
+                </Row>
               </Card>
             </Col>
-            <Col xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
-              <Card>loading info...</Card>
-            </Col>
-          </Row>
-        ) : (
-          <Row>
-            <Col xs={12} sm={12} md={3} lg={3} xl={3} xxl={3}>
+            {/* <Col xs={12} sm={12} md={3} lg={3} xl={3} xxl={3}>
               <Card>
                 <Card.Img src={characterData.img} alt={`Picture of ${characterData.name}`} />
               </Card>
             </Col>
             <Col xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
               <Card>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe accusantium nostrum accusamus dolore
-                voluptates nulla unde omnis rem, nesciunt voluptas minima temporibus, maiores magni error aut quasi
-                odio, excepturi iusto optio? Quo deleniti, iste provident id unde itaque porro? Eos minus facilis
-                doloribus nulla dignissimos. Asperiores labore mollitia sit dolores modi atque, accusantium consectetur
-                earum cupiditate praesentium. Iusto sit, sunt quisquam a laudantium ullam consectetur modi dolorem
-                reprehenderit corrupti, omnis voluptatibus cupiditate, facere quas culpa voluptatum magni. Praesentium
-                magnam nisi incidunt? Adipisci necessitatibus ipsam, quia eaque ipsa esse laboriosam nobis quasi
-                recusandae, blanditiis quam, excepturi dolorum suscipit! Vero, atque velit!
+                <Table>
+                  <tbody>
+                    <tr>
+                      <td>Name: {characterData.name}</td>
+                    </tr>
+                    <tr>
+                      <td>Nickname: {characterData.nickname}</td>
+                    </tr>
+                    <tr>
+                      <td>Birthday: {characterData.birthday}</td>
+                    </tr>
+                    <tr>
+                      <td>Occupation: {characterData.occupation}</td>
+                    </tr>
+                    <tr>
+                      <td>Portrayed by: {characterData.portrayed}</td>
+                    </tr>
+                    <tr>
+                      <td>Status: {characterData.status}</td>
+                    </tr>
+                    <tr>
+                      <td>Appears in: {characterData.category}</td>
+                    </tr>
+                    <tr>
+                      <td>Appears in the TV Shows: {characterData.category}</td>
+                    </tr>
+                    <tr>
+                      <td>Appears in the Breaking Bad Seasons: {characterData.appearance}</td>
+                    </tr>
+                  </tbody>
+                </Table>
               </Card>
-            </Col>
+            </Col> */}
           </Row>
         )}
       </div>
