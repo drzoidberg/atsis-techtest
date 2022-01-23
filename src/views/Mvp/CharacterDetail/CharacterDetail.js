8ade8fa4bd228a89736da1ad47553bad481d1087
Jorge Vicens Valor <gromit.jvv@gmail.com>
2022-01-23 15:22:44 +0100

    wip: CharacterDetail - building interface

--- src/views/Mvp/CharacterDetail/CharacterDetail.js
+++ src/views/Mvp/CharacterDetail/CharacterDetail.js
@@ -46,17 +46,12 @@ function CharacterDetail() {
 
   console.log(characterData)
 
-  //   characterData.occupation.map((occupation, index) => {
-  //     return index === characterData.occupation.length - 1 ? '' : ', '
-  //   })
-  // }
-
   return (
     <Container className='d-flex min-vh-100'>
       <div className='mt-4'>
         <Row className='mt-5 mb-3'>
           <div className='d-flex justify-content-between align-items-center'>
-            <h1 className='home-title text-center'>Character Details</h1>
+            <h1 className='home-title text-center mb-0'>Character Details</h1>
             <GenericButton onClick={history.goBack} variant='primary' contents={i18n[`uiTexts${LSLang}`].goBack} />
           </div>
         </Row>
@@ -66,63 +61,64 @@ function CharacterDetail() {
         ) : !isLoaded ? (
           <p>loading content...</p>
         ) : (
-          <Row xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
-            <Col className='mb-3'>
-              <Card className='h-100'>
-                <Row className='g-0'>
-                  <Col xs={12} sm={12} md={12} lg={3} xl={3} xxl={3}>
-                    <Card.Img
-                      rounded
-                      variant='top'
-                      className='m-2'
-                      src={characterData.img}
-                      alt={`Picture of ${characterData.img}`}
-                    />
-                  </Col>
-                  <Col xs={12} sm={12} md={12} lg={9} xl={9} xxl={9}>
-                    <Card.Body className='d-flex flex-column'>
-                      <Row>
-                        <Col xs={12} sm={12} md={12} lg={3} xl={3} xxl={3} className='d-flex justify-content-end'>
-                          <span className='text-muted'>Name:</span>
-                        </Col>
-                        <Col xs={12} sm={12} md={12} lg={9} xl={9} xxl={9} className='p-0'>
-                          {characterData.name}
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col xs={12} sm={12} md={12} lg={3} xl={3} xxl={3} className='d-flex justify-content-end'>
-                          <span className='text-muted'>Nickname:</span>
-                        </Col>
-                        <Col xs={12} sm={12} md={12} lg={9} xl={9} xxl={9} className='p-0'>
-                          {characterData.nickname}
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col xs={12} sm={12} md={12} lg={3} xl={3} xxl={3} className='d-flex justify-content-end'>
-                          <span className='text-muted'>Birthday:</span>
-                        </Col>
-                        <Col xs={12} sm={12} md={12} lg={9} xl={9} xxl={9} className='p-0'>
-                          {characterData.birthday}
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col xs={12} sm={12} md={12} lg={3} xl={3} xxl={3} className='d-flex justify-content-end'>
-                          <span className='text-muted'>
-                            {characterData.occupation.length > 1 ? 'Occupations:' : 'Occupation:'}
-                          </span>
-                        </Col>
-                        <Col xs={12} sm={12} md={12} lg={9} xl={9} xxl={9} className='p-0'>
-                          {characterData.occupation.length > 1
-                            ? characterData.occupation.map((occupation, index) => (
-                                <span>
-                                  {occupation}
-                                  {index === characterData.occupation.length - 1 ? '' : ', '}{' '}
-                                </span>
-                              ))
-                            : characterData.occupation}
-                        </Col>
-                      </Row>
-                      {/* <Button
+          <Row>
+            <Col xs={12} sm={12} md={12} lg={3} xl={3} xxl={3}>
+              <Image fluid rounded src={characterData.img} alt={`Picture of ${characterData.img}`} />
+            </Col>
+            <Col xs={12} sm={12} md={12} lg={9} xl={9} xxl={9}>
+              <Card>
+                <Card.Body>
+                  <Row>
+                    <Col xs={5} sm={5} md={5} lg={3} xl={3} xxl={3} className='d-flex justify-content-end'>
+                      <span className='text-muted'>Name:</span>
+                    </Col>
+                    <Col xs={7} sm={7} md={7} lg={3} xl={3} xxl={3} className='p-0'>
+                      {characterData.name}
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col xs={5} sm={5} md={5} lg={3} xl={3} xxl={3} className='d-flex justify-content-end'>
+                      <span className='text-muted'>Nickname:</span>
+                    </Col>
+                    <Col xs={7} sm={7} md={7} lg={3} xl={3} xxl={3} className='p-0'>
+                      {characterData.nickname}
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col xs={5} sm={5} md={5} lg={3} xl={3} xxl={3} className='d-flex justify-content-end'>
+                      <span className='text-muted'>Birthday:</span>
+                    </Col>
+                    <Col xs={7} sm={7} md={7} lg={3} xl={3} xxl={3} className='p-0'>
+                      {characterData.birthday}
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col xs={5} sm={5} md={5} lg={3} xl={3} xxl={3} className='d-flex justify-content-end'>
+                      <span className='text-muted'>
+                        {characterData.occupation.length > 1 ? 'Occupations:' : 'Occupation:'}
+                      </span>
+                    </Col>
+                    <Col xs={7} sm={7} md={7} lg={3} xl={3} xxl={3} className='p-0'>
+                      {characterData.occupation.length > 1
+                        ? characterData.occupation.map((occupation, index) => (
+                            <span>
+                              {occupation}
+                              {index === characterData.occupation.length - 1 ? '' : ', '}{' '}
+                            </span>
+                          ))
+                        : characterData.occupation}
+                    </Col>
+                  </Row>
+                  <hr className='mx-5' />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam numquam dicta praesentium est animi
+                  autem tempore reprehenderit facilis excepturi provident porro aspernatur molestiae, incidunt culpa
+                  sunt facere, suscipit, corrupti omnis. Recusandae facere, beatae quibusdam voluptas quos alias amet
+                  quisquam. Laboriosam dolorem, maiores quis expedita esse impedit deserunt doloribus tempora culpa enim
+                  magnam voluptatum modi. Tempore maiores ipsam magni. Sunt perspiciatis obcaecati delectus dolore
+                  possimus. Maxime repudiandae, voluptates laboriosam debitis quidem ad magnam inventore vel in eum
+                  ducimus molestiae expedita quos maiores architecto similique est? Atque magni quia tenetur temporibus
+                  voluptates odio voluptas deserunt aspernatur fugit id explicabo sapiente, incidunt quod.
+                  {/* <Button
                     variant='primary'
                     as={Link}
                     to={`/mvp/character-detail/${character.char_id}`}
@@ -130,51 +126,9 @@ function CharacterDetail() {
                   >
                     {i18n[`uiTexts${LSLang}`].characterDetail}
                   </Button> */}
-                    </Card.Body>
-                  </Col>
-                </Row>
+                </Card.Body>
               </Card>
             </Col>
-            {/* <Col xs={12} sm={12} md={3} lg={3} xl={3} xxl={3}>
-              <Card>
-                <Card.Img src={characterData.img} alt={`Picture of ${characterData.name}`} />
-              </Card>
-            </Col>
-            <Col xs={12} sm={12} md={9} lg={9} xl={9} xxl={9}>
-              <Card>
-                <Table>
-                  <tbody>
-                    <tr>
-                      <td>Name: {characterData.name}</td>
-                    </tr>
-                    <tr>
-                      <td>Nickname: {characterData.nickname}</td>
-                    </tr>
-                    <tr>
-                      <td>Birthday: {characterData.birthday}</td>
-                    </tr>
-                    <tr>
-                      <td>Occupation: {characterData.occupation}</td>
-                    </tr>
-                    <tr>
-                      <td>Portrayed by: {characterData.portrayed}</td>
-                    </tr>
-                    <tr>
-                      <td>Status: {characterData.status}</td>
-                    </tr>
-                    <tr>
-                      <td>Appears in: {characterData.category}</td>
-                    </tr>
-                    <tr>
-                      <td>Appears in the TV Shows: {characterData.category}</td>
-                    </tr>
-                    <tr>
-                      <td>Appears in the Breaking Bad Seasons: {characterData.appearance}</td>
-                    </tr>
-                  </tbody>
-                </Table>
-              </Card>
-            </Col> */}
           </Row>
         )}
       </div>
