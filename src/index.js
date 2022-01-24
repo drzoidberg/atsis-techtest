3660c032b36ee1fffd2f828f0d0426bac861b382
Jorge Vicens Valor <gromit.jvv@gmail.com>
2022-01-24 20:09:27 +0100

    FIX: dsbled redux thunk

--- src/index.js
+++ src/index.js
@@ -9,12 +9,13 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.module.scss'
 
-const composedEnhancer = compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-)
+// const composedEnhancer = compose(
+//   applyMiddleware(thunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+// )
 
-const store = createStore(reducers, composedEnhancer)
+// const store = createStore(reducers, composedEnhancer)
+const store = createStore(reducers, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
